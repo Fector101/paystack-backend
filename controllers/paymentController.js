56a0d7f3d7461da8e39d3f8a12a0bb18730a18c9
Fabian <fabianjoseph063@gmail.com>
2025-07-16 05:56:04 -0700

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -14,7 +14,7 @@ console.log("email-->", email, amount)
     });
 
     const data = await response.json();
-    console.log(data)
+    
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: 'Initialization failed' });
